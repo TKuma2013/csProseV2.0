eb06b4000e0dbdab67435ae08b96dfe142a52cfc
TKuma2013 <tkumara2013@yahoo.com>
2017-10-04 20:49:20 -0400

    root

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('csProse')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('csProse', ['ui.router', 'ui.bootstrap', 'toastr']);
+
+})();
