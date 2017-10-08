81ab6de6e377f8c6fc1f516174fcf571c6a5cd36
TKuma2013 <tkumara2013@yahoo.com>
2017-10-08 14:33:48 -0400

    post_login_working

--- server.js
+++ server.js
@@ -1,14 +1,43 @@
 var express = require('express');
 var app=express();
 var bodyParser=require('body-parser');
+var mongoose = require('mongoose');
+//var database;
+
+
+var auth = require('./controllers/auth');
+var message=require('./controllers/message');
+
 
 app.use(bodyParser.json());
+app.use(function(req,res,next){
+    res.header("Access-Control-Allow-Origin","*");
+    res.header("Access-Control-Allow-Headers","Content-Type, Authorization");
+    next();
+})
 
-app.post('/api/message',function(req,res){
-   console.log(req.body);
-    res.status(200);
+app.get('/api/message',message.get);
+
+app.post('/api/message',message.post);
+
+app.post('/auth/register', auth.register);
+
+
+
+
+mongoose.connect("mongodb://localhost:27017/test",function(err,db){
+    if(!err)
+        {
+            console.log("we are connected to mongo");
+          //  GetMessages();
+          //  database=db;
+           // db.collection('messages').insertOne({'msg' : 'test'});
+
+        }
 
 })
+
+
 var server=app.listen(5000,function(){
     console.log('listening on port',server.address().port);
 
